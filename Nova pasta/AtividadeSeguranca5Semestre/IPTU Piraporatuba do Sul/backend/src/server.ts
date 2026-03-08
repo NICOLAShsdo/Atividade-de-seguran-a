c9ab1696d44cea33f68e7c2c7168a76d4ab70a3d
NICOLAShsdo <164954695+NICOLAShsdo@users.noreply.github.com>
2026-03-08 20:11:30 -0300

    Add files via upload

--- Nova pasta/AtividadeSeguranca5Semestre/IPTU Piraporatuba do Sul/backend/src/server.ts
+++ Nova pasta/AtividadeSeguranca5Semestre/IPTU Piraporatuba do Sul/backend/src/server.ts
@@ -1,18 +1,40 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+
 import userRoutes from "./routes/usuarioRoutes";
 import commentRoutes from "./routes/comentarioRoutes";
-import hackerMalvadao from "./routes/hackerMalvadaoRoutes";
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+/* Segurança de headers HTTP */
+app.use(helmet());
 
+/* Configuração segura de CORS */
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+/* Limite de tamanho de requisição */
+app.use(express.json({
+  limit: "10kb"
+}));
+
+/* Proteção contra spam e brute force */
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+});
+
+app.use(limiter);
+
+/* Rotas da aplicação */
 app.use("/usuario", userRoutes);
 app.use("/comentario", commentRoutes);
-app.use("/hacker-malvadao", hackerMalvadao);
 
 app.listen(3001, () => {
-    console.log("Servidor Vulnerável rodando na porta 3001");
+  console.log("Servidor rodando com configurações seguras na porta 3001");
 });
