3fff62d4ddccec2830963dd5c64da703924d4aa6
NICOLAShsdo <164954695+NICOLAShsdo@users.noreply.github.com>
2026-03-08 20:05:33 -0300

    Add files via upload

--- Nova pasta/AtividadeSeguranca5Semestre/IPTU Piraporatuba do Sul/backend/src/routes/comentarioRoutes.ts
+++ Nova pasta/AtividadeSeguranca5Semestre/IPTU Piraporatuba do Sul/backend/src/routes/comentarioRoutes.ts
@@ -1,9 +1,27 @@
 import { Router } from "express";
+import rateLimit from "express-rate-limit";
 import { criarComentario, listarComentarios } from "../controllers/comentarioController";
+import { autenticarToken } from "../middlewares/authMiddleware";
 
 const router = Router();
 
-router.post("/", criarComentario);
-router.get("/", listarComentarios);
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+});
+
+router.use(limiter);
+
+router.post(
+  "/",
+  autenticarToken,
+  criarComentario
+);
+
+router.get(
+  "/",
+  autenticarToken,
+  listarComentarios
+);
 
 export default router;
